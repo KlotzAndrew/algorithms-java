492088dc80d948e9e6be71667fd3929f464ebf12
Andrew Klotz <agc.klotz@gmail.com>
2018-01-15 23:26:02 -0500

    new app

---
+++ src/main/java/com/klotzandrew/app/App.java
@@ -0,0 +1,13 @@
+package com.klotzandrew.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
